10d067c1171bcd1c95d2a6b9cbaed98ee7cb2089
m <mike@proofsuite.com>
2019-07-21 04:06:58 +0900

    cmd update from gaia

--- cmd/zonecli/main.go
+++ cmd/zonecli/main.go
@@ -2,28 +2,67 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"path"
 
-	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/client/keys"
-	"github.com/cosmos/cosmos-sdk/client/lcd"
-	"github.com/cosmos/cosmos-sdk/client/rpc"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/version"
-	"github.com/cosmos/cosmos-sdk/x/auth"
-	authcmd "github.com/cosmos/cosmos-sdk/x/auth/client/cli"
-	authrest "github.com/cosmos/cosmos-sdk/x/auth/client/rest"
-	"github.com/cosmos/cosmos-sdk/x/bank"
-	bankcmd "github.com/cosmos/cosmos-sdk/x/bank/client/cli"
-
+	"github.com/rakyll/statik/fs"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
 	"github.com/tendermint/go-amino"
 	"github.com/tendermint/tendermint/libs/cli"
 
-	"github.com/cosmos/gaia/app"
+	"github.com/terra-project/core/app"
+	"github.com/terra-project/core/client/keys"
+	"github.com/terra-project/core/types/util"
+	"github.com/terra-project/core/version"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/lcd"
+	"github.com/cosmos/cosmos-sdk/client/rpc"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	at "github.com/cosmos/cosmos-sdk/x/auth"
+
+	txcustom "github.com/terra-project/core/client/tx"
+
+	authcustom "github.com/terra-project/core/x/auth/client/rest"
+	dist "github.com/terra-project/core/x/distribution/client/rest"
+	slashing "github.com/terra-project/core/x/slashing/client/rest"
+	staking "github.com/terra-project/core/x/staking/client/rest"
+
+	budget "github.com/terra-project/core/x/budget/client/rest"
+	market "github.com/terra-project/core/x/market/client/rest"
+	oracle "github.com/terra-project/core/x/oracle/client/rest"
+	pay "github.com/terra-project/core/x/pay/client/rest"
+	treasury "github.com/terra-project/core/x/treasury/client/rest"
+
+	bud "github.com/terra-project/core/x/budget"
+	mkt "github.com/terra-project/core/x/market"
+	ora "github.com/terra-project/core/x/oracle"
+	tre "github.com/terra-project/core/x/treasury"
+
+	dt "github.com/cosmos/cosmos-sdk/x/distribution"
+	sl "github.com/cosmos/cosmos-sdk/x/slashing"
+	st "github.com/cosmos/cosmos-sdk/x/staking"
+
+	auth "github.com/cosmos/cosmos-sdk/x/auth/client/rest"
+
+	authcmd "github.com/cosmos/cosmos-sdk/x/auth/client/cli"
+	paycmd "github.com/terra-project/core/x/pay/client/cli"
+
+	budgetClient "github.com/terra-project/core/x/budget/client"
+	distClient "github.com/terra-project/core/x/distribution/client"
+	marketClient "github.com/terra-project/core/x/market/client"
+	oracleClient "github.com/terra-project/core/x/oracle/client"
+	slashingClient "github.com/terra-project/core/x/slashing/client"
+	stakingClient "github.com/terra-project/core/x/staking/client"
+	treasuryClient "github.com/terra-project/core/x/treasury/client"
+
+	crisisClient "github.com/cosmos/cosmos-sdk/x/crisis/client"
+
+	_ "github.com/terra-project/core/client/lcd/statik"
 )
 
 func main() {
@@ -35,14 +74,29 @@ func main() {
 
 	// Read in the configuration file for the sdk
 	config := sdk.GetConfig()
-	config.SetBech32PrefixForAccount(sdk.Bech32PrefixAccAddr, sdk.Bech32PrefixAccPub)
-	config.SetBech32PrefixForValidator(sdk.Bech32PrefixValAddr, sdk.Bech32PrefixValPub)
-	config.SetBech32PrefixForConsensusNode(sdk.Bech32PrefixConsAddr, sdk.Bech32PrefixConsPub)
+	config.SetCoinType(util.CoinType)
+	config.SetFullFundraiserPath(util.FullFundraiserPath)
+	config.SetBech32PrefixForAccount(util.Bech32PrefixAccAddr, util.Bech32PrefixAccPub)
+	config.SetBech32PrefixForValidator(util.Bech32PrefixValAddr, util.Bech32PrefixValPub)
+	config.SetBech32PrefixForConsensusNode(util.Bech32PrefixConsAddr, util.Bech32PrefixConsPub)
 	config.Seal()
 
 	// TODO: setup keybase, viper object, etc. to be passed into
 	// the below functions and eliminate global vars, like we do
 	// with the cdc
+
+	// Module clients hold cli commnads (tx,query) and lcd routes
+	// TODO: Make the lcd command take a list of ModuleClient
+	mc := []sdk.ModuleClients{
+		distClient.NewModuleClient(dt.StoreKey, cdc),
+		stakingClient.NewModuleClient(st.StoreKey, cdc),
+		slashingClient.NewModuleClient(sl.StoreKey, cdc),
+		oracleClient.NewModuleClient(ora.StoreKey, cdc),
+		treasuryClient.NewModuleClient(tre.StoreKey, cdc),
+		budgetClient.NewModuleClient(bud.StoreKey, cdc),
+		marketClient.NewModuleClient(mkt.StoreKey, cdc),
+		crisisClient.NewModuleClient(sl.StoreKey, cdc),
+	}
 
 	rootCmd := &cobra.Command{
 		Use:   "zonecli",
@@ -59,19 +113,19 @@ func main() {
 	rootCmd.AddCommand(
 		rpc.StatusCommand(),
 		client.ConfigCmd(app.DefaultCLIHome),
-		queryCmd(cdc),
-		txCmd(cdc),
+		queryCmd(cdc, mc),
+		txCmd(cdc, mc),
 		client.LineBreak,
 		lcd.ServeCommand(cdc, registerRoutes),
 		client.LineBreak,
 		keys.Commands(),
 		client.LineBreak,
-		version.Cmd,
+		version.VersionCmd,
 		client.NewCompletionCmd(rootCmd, true),
 	)
 
 	// Add flags and prefix all env exposed with GA
-	executor := cli.PrepareMainCmd(rootCmd, "GA", app.DefaultCLIHome)
+	executor := cli.PrepareMainCmd(rootCmd, "TE", app.DefaultCLIHome)
 
 	err := executor.Execute()
 	if err != nil {
@@ -80,7 +134,7 @@ func main() {
 	}
 }
 
-func queryCmd(cdc *amino.Codec) *cobra.Command {
+func queryCmd(cdc *amino.Codec, mc []sdk.ModuleClients) *cobra.Command {
 	queryCmd := &cobra.Command{
 		Use:     "query",
 		Aliases: []string{"q"},
@@ -88,62 +142,83 @@ func queryCmd(cdc *amino.Codec) *cobra.Command {
 	}
 
 	queryCmd.AddCommand(
-		authcmd.GetAccountCmd(cdc),
-		client.LineBreak,
 		rpc.ValidatorCommand(cdc),
 		rpc.BlockCommand(),
-		authcmd.QueryTxsByEventsCmd(cdc),
-		authcmd.QueryTxCmd(cdc),
+		tx.SearchTxCmd(cdc),
+		tx.QueryTxCmd(cdc),
 		client.LineBreak,
+		authcmd.GetAccountCmd(at.StoreKey, cdc),
 	)
 
-	// add modules' query commands
-	app.ModuleBasics.AddQueryCommands(queryCmd, cdc)
+	for _, m := range mc {
+		mQueryCmd := m.GetQueryCmd()
+		if mQueryCmd != nil {
+			queryCmd.AddCommand(mQueryCmd)
+		}
+	}
 
 	return queryCmd
 }
 
-func txCmd(cdc *amino.Codec) *cobra.Command {
+func txCmd(cdc *amino.Codec, mc []sdk.ModuleClients) *cobra.Command {
 	txCmd := &cobra.Command{
 		Use:   "tx",
 		Short: "Transactions subcommands",
 	}
 
 	txCmd.AddCommand(
-		bankcmd.SendTxCmd(cdc),
+		paycmd.SendTxCmd(cdc),
 		client.LineBreak,
 		authcmd.GetSignCommand(cdc),
 		authcmd.GetMultiSignCommand(cdc),
-		client.LineBreak,
-		authcmd.GetBroadcastCommand(cdc),
-		authcmd.GetEncodeCommand(cdc),
+		tx.GetBroadcastCommand(cdc),
+		tx.GetEncodeCommand(cdc),
 		client.LineBreak,
 	)
 
-	// add modules' tx commands
-	app.ModuleBasics.AddTxCommands(txCmd, cdc)
-
-	// remove auth and bank commands as they're mounted under the root tx command
-	var cmdsToRemove []*cobra.Command
-
-	for _, cmd := range txCmd.Commands() {
-		if cmd.Use == auth.ModuleName || cmd.Use == bank.ModuleName {
-			cmdsToRemove = append(cmdsToRemove, cmd)
-		}
+	for _, m := range mc {
+		txCmd.AddCommand(m.GetTxCmd())
 	}
 
-	txCmd.RemoveCommand(cmdsToRemove...)
-
 	return txCmd
+}
+
+// CLIVersionRequestHandler cli version REST handler endpoint
+func CLIVersionRequestHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	_, _ = w.Write([]byte(fmt.Sprintf("{\"version\": \"%s\"}", version.Version)))
 }
 
 // registerRoutes registers the routes from the different modules for the LCD.
 // NOTE: details on the routes added for each module are in the module documentation
 // NOTE: If making updates here you also need to update the test helper in client/lcd/test_helper.go
 func registerRoutes(rs *lcd.RestServer) {
-	client.RegisterRoutes(rs.CliCtx, rs.Mux)
-	authrest.RegisterTxRoutes(rs.CliCtx, rs.Mux)
-	app.ModuleBasics.RegisterRESTRoutes(rs.CliCtx, rs.Mux)
+
+	rs.Mux.HandleFunc("/version", CLIVersionRequestHandler).Methods("GET")
+
+	registerSwaggerUI(rs)
+	rpc.RegisterRoutes(rs.CliCtx, rs.Mux)
+	txcustom.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc)
+	auth.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc, at.StoreKey)
+	dist.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc, dt.StoreKey)
+	staking.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc, rs.KeyBase)
+	slashing.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc, rs.KeyBase)
+
+	authcustom.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc, rs.KeyBase)
+	pay.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc, rs.KeyBase)
+	oracle.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc)
+	treasury.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc)
+	market.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc)
+	budget.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc)
+}
+
+func registerSwaggerUI(rs *lcd.RestServer) {
+	statikFS, err := fs.New()
+	if err != nil {
+		panic(err)
+	}
+	staticServer := http.FileServer(statikFS)
+	rs.Mux.PathPrefix("/swagger-ui/").Handler(http.StripPrefix("/swagger-ui/", staticServer))
 }
 
 func initConfig(cmd *cobra.Command) error {
--- cmd/zoned/main.go
+++ cmd/zoned/main.go
@@ -4,6 +4,13 @@ import (
 	"encoding/json"
 	"io"
 
+	"github.com/terra-project/core/types/util"
+	"github.com/terra-project/core/version"
+
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/store"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
@@ -13,85 +20,79 @@ import (
 	"github.com/tendermint/tendermint/libs/log"
 	tmtypes "github.com/tendermint/tendermint/types"
 
-	"github.com/cosmos/gaia/app"
+	"github.com/terra-project/core/app"
+	terraInit "github.com/terra-project/core/cmd/init"
 
-	"github.com/cosmos/cosmos-sdk/baseapp"
-	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/server"
-	"github.com/cosmos/cosmos-sdk/store"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/genaccounts"
-	genaccscli "github.com/cosmos/cosmos-sdk/x/genaccounts/client/cli"
-	genutilcli "github.com/cosmos/cosmos-sdk/x/genutil/client/cli"
-	"github.com/cosmos/cosmos-sdk/x/staking"
+	terraserver "github.com/terra-project/core/server"
 )
 
-// gaiad custom flags
-const flagInvCheckPeriod = "inv-check-period"
+const flagAssertInvariantsBlockly = "assert-invariants-blockly"
 
-var invCheckPeriod uint
+var assertInvariantsBlockly bool
 
 func main() {
 	cdc := app.MakeCodec()
 
 	config := sdk.GetConfig()
-	config.SetBech32PrefixForAccount(sdk.Bech32PrefixAccAddr, sdk.Bech32PrefixAccPub)
-	config.SetBech32PrefixForValidator(sdk.Bech32PrefixValAddr, sdk.Bech32PrefixValPub)
-	config.SetBech32PrefixForConsensusNode(sdk.Bech32PrefixConsAddr, sdk.Bech32PrefixConsPub)
+	config.SetCoinType(util.CoinType)
+	config.SetFullFundraiserPath(util.FullFundraiserPath)
+	config.SetBech32PrefixForAccount(util.Bech32PrefixAccAddr, util.Bech32PrefixAccPub)
+	config.SetBech32PrefixForValidator(util.Bech32PrefixValAddr, util.Bech32PrefixValPub)
+	config.SetBech32PrefixForConsensusNode(util.Bech32PrefixConsAddr, util.Bech32PrefixConsPub)
 	config.Seal()
 
 	ctx := server.NewDefaultContext()
 	cobra.EnableCommandSorting = false
 	rootCmd := &cobra.Command{
 		Use:               "zoned",
-		Short:             "Zone Daemon (server)",
-		PersistentPreRunE: server.PersistentPreRunEFn(ctx),
+		Short:             "Cosmos Zone Daemon (server)",
+		PersistentPreRunE: terraserver.PersistentPreRunEFn(ctx),
 	}
-
-	rootCmd.AddCommand(genutilcli.InitCmd(ctx, cdc, app.ModuleBasics, app.DefaultNodeHome))
-	rootCmd.AddCommand(genutilcli.CollectGenTxsCmd(ctx, cdc, genaccounts.AppModuleBasic{}, app.DefaultNodeHome))
-	rootCmd.AddCommand(genutilcli.MigrateGenesisCmd(ctx, cdc))
-	rootCmd.AddCommand(genutilcli.GenTxCmd(ctx, cdc, app.ModuleBasics, staking.AppModuleBasic{},
-		genaccounts.AppModuleBasic{}, app.DefaultNodeHome, app.DefaultCLIHome))
-	rootCmd.AddCommand(genutilcli.ValidateGenesisCmd(ctx, cdc, app.ModuleBasics))
-	rootCmd.AddCommand(genaccscli.AddGenesisAccountCmd(ctx, cdc, app.DefaultNodeHome, app.DefaultCLIHome))
+	rootCmd.AddCommand(terraInit.InitCmd(ctx, cdc))
+	rootCmd.AddCommand(terraInit.CollectGenTxsCmd(ctx, cdc))
+	rootCmd.AddCommand(terraInit.TestnetFilesCmd(ctx, cdc))
+	rootCmd.AddCommand(terraInit.GenTxCmd(ctx, cdc))
+	rootCmd.AddCommand(terraInit.AddGenesisAccountCmd(ctx, cdc))
+	rootCmd.AddCommand(terraInit.ValidateGenesisCmd(ctx, cdc))
 	rootCmd.AddCommand(client.NewCompletionCmd(rootCmd, true))
-	rootCmd.AddCommand(testnetCmd(ctx, cdc, app.ModuleBasics, genaccounts.AppModuleBasic{}))
-	rootCmd.AddCommand(replayCmd())
+
+	// preempting version command
+	rootCmd.AddCommand(version.VersionCmd)
 
 	server.AddCommands(ctx, cdc, rootCmd, newApp, exportAppStateAndTMValidators)
 
 	// prepare and add flags
-	executor := cli.PrepareBaseCmd(rootCmd, "GA", app.DefaultNodeHome)
-	rootCmd.PersistentFlags().UintVar(&invCheckPeriod, flagInvCheckPeriod,
-		0, "Assert registered invariants every N blocks")
+	executor := cli.PrepareBaseCmd(rootCmd, "TE", app.DefaultNodeHome)
+	rootCmd.PersistentFlags().BoolVar(&assertInvariantsBlockly, flagAssertInvariantsBlockly,
+		false, "Assert registered invariants on a blockly basis")
 	err := executor.Execute()
 	if err != nil {
+		// handle with #870
 		panic(err)
 	}
 }
 
 func newApp(logger log.Logger, db dbm.DB, traceStore io.Writer) abci.Application {
-	return app.NewGaiaApp(
-		logger, db, traceStore, true, invCheckPeriod,
+	return app.NewTerraApp(
+		logger, db, traceStore, true, assertInvariantsBlockly,
 		baseapp.SetPruning(store.NewPruningOptionsFromString(viper.GetString("pruning"))),
 		baseapp.SetMinGasPrices(viper.GetString(server.FlagMinGasPrices)),
-		baseapp.SetHaltHeight(uint64(viper.GetInt(server.FlagHaltHeight))),
 	)
 }
 
 func exportAppStateAndTMValidators(
 	logger log.Logger, db dbm.DB, traceStore io.Writer, height int64, forZeroHeight bool, jailWhiteList []string,
 ) (json.RawMessage, []tmtypes.GenesisValidator, error) {
-
 	if height != -1 {
-		gApp := app.NewGaiaApp(logger, db, traceStore, false, uint(1))
-		err := gApp.LoadHeight(height)
+		tApp := app.NewTerraApp(logger, db, traceStore, false, false)
+		err := tApp.LoadHeight(height)
 		if err != nil {
 			return nil, nil, err
 		}
-		return gApp.ExportAppStateAndValidators(forZeroHeight, jailWhiteList)
+		return tApp.ExportAppStateAndValidators(forZeroHeight, jailWhiteList)
 	}
-	gApp := app.NewGaiaApp(logger, db, traceStore, true, uint(1))
-	return gApp.ExportAppStateAndValidators(forZeroHeight, jailWhiteList)
+	tApp := app.NewTerraApp(logger, db, traceStore, true, false)
+	return tApp.ExportAppStateAndValidators(forZeroHeight, jailWhiteList)
 }
